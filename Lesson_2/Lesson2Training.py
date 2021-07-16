152c53dc7fcc4a3a9b25641b9db8eb289abfa291
nsheeh01 <44305411+nsheeh01@users.noreply.github.com>
2021-07-16 10:58:42 -0400

    Create Lesson2Training.py

---
+++ Lesson_2/Lesson2Training.py
@@ -0,0 +1,94 @@
+# Last modified: July 7 '21
+# PetTraining Supervised (K-Nearest Neighbors)
+# PetTrainingSupervised is an AI module which provides PetTrainingKNN, a class for an EV3 running micropython.
+
+import hub,utime
+
+class PetTrainingKNN:
+    # PetTrainingKNN uses a 1-dimenional K-Nearest Neighbor algorithm for training
+    # a robot to detect two different lengths of button presses corresponding to a
+    # "pat" and "stroke" for a puppy reacting to different pet lengths
+    
+    def __init__(self, train_num):
+        # Initialize the touch sensor, and timer
+        # Train_num is number of training strokes and pats
+        self.touch = hub.port.F.device      ##### <----------- REPLACE WITH YOUR FORCE SENSOR HUB PORT ######
+        self.pats = [0]*train_num 
+        self.strokes = [0]*train_num
+        
+    def button_timer(self):
+        # Record and measure the length of the next button press
+        while not self.touch.get()[0]>0:
+            utime.sleep(0.01)
+            if hub.button.center.is_pressed():
+                return "END"
+        start_time = utime.time()
+        while self.touch.get()[0]>0:
+            utime.sleep(0.001)
+        end_time = utime.time()
+        time = end_time - start_time
+        return time
+    
+    def record_examples(self, num, ptype):
+        # Record num examples and store them in the example array matching the ptype
+        print('** TRAIN %ss **' % (ptype))
+        if ptype == "pat":
+            array = self.pats
+        elif ptype == "stroke":
+            array = self.strokes
+        for i in range(num):
+            print('%s example %ss' % (num - i, ptype))
+            btime = self.button_timer()
+            if btime == "END":
+                break
+            else:
+                array[i] = btime
+                
+    def distance(self, a, b):
+        # Calculate the one dimensional distance between point a and point b
+        return abs(a - b)
+    
+    def k_nearest_neighbor_prediction(self, sample, k):
+        # Predict the class of the provided sample using the classes of K trained cases
+        distances = [(0, 0)]*(len(self.pats)+len(self.strokes))
+        i = 0
+        for pat_data in self.pats:
+            distances[i] = (self.distance(sample, pat_data), 0)
+            i += 1
+        for stroke_data in self.strokes:
+            distances[i] = (self.distance(sample, stroke_data), 1)
+            i += 1
+        distances.sort()
+        sum = 0
+        for i in range(k):
+            sum += distances[i][1]
+        if sum < (k-sum): # return 0 if mostly 0s
+            return "pat"
+        else:
+            return "stroke"
+        
+    def report(self):
+        # Create a print report of training data
+        
+        # Find pat and stroke means
+        sumpats = sum(self.pats)
+        sumstrokes = sum(self.strokes)
+        numpats = len(self.pats)
+        numstrokes = len(self.strokes)
+        meanpats = round(sumpats/numpats)
+        meanstrokes = round(sumstrokes/numstrokes)
+        
+        # Print pat and stroke lists and means
+        print("*"*20)
+        print('Pats: ' + str(self.pats))
+        print('Mean of %d pats is %s' % (numpats, meanpats))
+        print("*"*20)
+        print('Strokes: ' + str(self.strokes))
+        print('Mean of %d strokes is %s' % (numstrokes, meanstrokes))
+        print("*"*20)
+        
+    def forget(self):
+        # Clear training and model
+        self.pats = [0]*train_num
+        self.strokes = [0]*train_num
+        
