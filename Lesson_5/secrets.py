4ee35275be9275f0c9872846416f2b7f0cc770e3
nsheeh01 <44305411+nsheeh01@users.noreply.github.com>
2021-07-21 09:53:08 -0400

    Create secrets.py

---
+++ Lesson_5/secrets.py
@@ -0,0 +1,7 @@
+## Secrets.py 
+# Fill in with your SSID, network password, airtable app key, and airtable docID!
+
+SSID="" # Network SSID. 
+KEY=""  # Network password
+tsATappkey = "" # AirTable app key
+ATdocID = "" #AirTable docID
